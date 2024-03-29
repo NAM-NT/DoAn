b2ce6add8b40c556b7c869ee28b54436664ef3f2
NAM-NT <nguyenthenamk49a2@gmail.com>
2024-03-29 14:07:18 +0700

    iam nam

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import RootComponent from './screens/root';
+import LoginScreen from './screens/login'
+
+
+export default function App() {
+  return (
+    <LoginScreen/>
+  );
+}
+
